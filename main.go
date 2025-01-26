c66ebcb6a9efabf14b7c8bf0e92ff884fe342408
s.panarin <svpanarin123@gmail.com>
2025-01-26 15:57:16 +0400

    Horse

--- main.go
+++ main.go
@@ -12,23 +12,22 @@ func main() {
 		y2 int
 	)
 	fmt.Scan(&x1, &y1, &x2, &y2)
-	var isValid bool
 
 	if x1+2 == x2 && (y1+1 == y2 || y1-1 == y2) {
-		isValid = true
+		fmt.Println("ДА")
+		return
 	}
 	if x1-2 == x2 && (y1+1 == y2 || y1-1 == y2) {
-		isValid = true
+		fmt.Println("ДА")
+		return
 	}
 	if y1+2 == y2 && (x1+1 == x2 || x1-1 == x2) {
-		isValid = true
+		fmt.Println("ДА")
+		return
 	}
 	if y1-2 == y2 && (x1+1 == x2 || x1-1 == x2) {
-		isValid = true
-	}
-	if isValid {
 		fmt.Println("ДА")
-	} else {
-		fmt.Println("НЕТ")
+		return
 	}
+	fmt.Println("НЕТ")
 }
